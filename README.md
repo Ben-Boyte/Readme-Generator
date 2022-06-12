f30dbc05a49707a55be4d9a1dbac644ba58a063d
Ben-Boyte <zissette1@gmail.com>
2022-06-12 17:18:12 -0500

    added most functionality and licenses into indexjs and generatemarkdown

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
-# Professional README Generator Starter Code
-
-[How to create a Professional README](https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide)
+# Ben
+## Description
+Ben
+![badge](https://img.shields.io/badge/license-GNUAGPLv3-brightorange)
+## Table of Contents
+  * [Questions](#questions)
+  * [License](#license)
+    
+    
+      
+## Questions
+      
+  _For further questions:_
+  undefined
+  
+  _Contact Info:_
+  GitHub: [Ben](https://github.com/Ben)
+  Email: [Ben](mailto:Ben)
+    
+## License
+      
+  _This application has the GNU AGPLv3._
+      
+  For more information please view the [license description](https://choosealicense.com/licenses/agpl-3.0/).
+  
+  
---
+++ index.js
@@ -0,0 +1,153 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
+const util = require('util');
+
+const questions = [{
+  type: 'input',
+  name: 'title',
+  message: 'What is the title of your repository?',
+  validate: nameInput => {
+    if (nameInput) {
+      return true;
+    } else {
+      console.log('Please enter a repository.');
+      return false;
+    }
+  }
+},
+{
+  type: 'input',
+  name: 'description',
+  message: 'Give your repository a description',
+  validate: nameInput => {
+    if (nameInput) {
+      return true;
+    } else {
+      console.log('Please enter a description.');
+      return false;
+    }
+  }
+  },
+{
+  type: 'confirm',
+  name: 'confirmInstallation',
+  message: 'Is there an installation?'
+  },
+{
+  type: 'input',
+  name: 'installation',
+  message: 'Please list installation instructions.',
+  when: ({ confirmInstallation }) => {
+    if (confirmInstallation) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+},
+
+{
+  type: 'confirm',
+  name: 'confirmUsage',
+  message: 'Do you want to give an instruction on how to use your application?'
+},
+{
+  type: 'input',
+  name: 'instructions',
+  message: 'Please list instructions.',
+  when: ({ confirmUsage }) => {
+    if (confirmUsage) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+},
+
+{
+  type: 'confirm',
+  name: 'testConfirm',
+  message: 'Is testing available?'
+},
+{
+  type: 'input',
+  name: 'testing',
+  message: 'Please explain how your application can be tested.',
+  when: ({ testConfirm }) => {
+    if (testConfirm) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+},
+{ 
+  type: 'list',
+  name: 'license',
+  message: 'Please choose a license.',
+  choices: ['GNU AGPLv3', 'GNU GPLv3',
+    'GNU LGPLv3', 'Mozilla Public License 2.0',
+    'Apache License 2.0', 'MIT License', 'Boost Software License 1.0',
+    'The Unlicense'],
+  validate: nameInput => {
+    if (nameInput) {
+      return true;
+    } else {
+      console.log('Please select a license.');
+      return false;
+    }
+  }
+},
+{
+  type: 'input',
+  name: 'username',
+  message: 'What is your GitHub username?',
+  validate: nameInput => {
+    if (nameInput) {
+      return true;
+    } else {
+      console.log('Please enter GitHub username.');
+      return false;
+    }
+  }
+},
+{
+  type: 'input',
+  name: 'email',
+  message: 'What is your email address? (Required)',
+  validate: nameInput => {
+    if (nameInput) {
+      return true;
+    } else {
+      console.log('Please enter your email.');
+      return false;
+    }
+  }
+}];
+
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, error => {
+    if (error) {
+      return console.log('Sorry there was an error : ' + error);
+    }
+  })
+}
+
+
+const createReadMe = util.promisify(writeToFile);
+
+async function init() {
+  try {
+    const userInput = await inquirer.prompt(questions);
+    console.log('Thank you! The info is being made into a README file!', userInput);
+    const myReadme = generateMarkdown(userInput);
+    console.log(myReadme);
+    await createReadMe('README.md', myReadme);
+    
+  } catch (error) {
+    console.log('Error.' + error);
+  }
+};
+
+init();
